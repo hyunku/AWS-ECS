310a957b6c8d286e714e23a59cf69b979a41e3ab
hyunku <kahk000@naver.com>
2022-05-13 14:22:21 +0900

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -1,6 +1,13 @@
 #package import
-import json
+import os
+import sys
+import pandas as pd
+import numpy as np
+from scipy import signal
 import mne
-import sklearn
-import matplotlib
-import pandas
+import matplotlib.pyplot as plt
+from mpl_toolkits.axes_grid1 import make_axes_locatable
+
+
+def lambda_handler(event, context):
+    print("test")
